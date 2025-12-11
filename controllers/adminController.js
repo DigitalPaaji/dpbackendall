b2619735ca3e85eaa46f785e545d7ed1a1968715
jonty1231 <jontythakur68@gmail.com>
2025-12-11 16:16:58 +0530

    Add

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -39,8 +39,8 @@ const createAdmin = async (req, res) => {
     res.cookie("adminToken", token, {
       path: "/",
       httpOnly: true,
-      secure: true,
-      sameSite: "lax",
+      secure: process.env.WORK=="production"? true:false,
+      sameSite: process.env.WORK=="production"? "none": "lax",
       expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 20),
     });
 
