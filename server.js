49ed8b5cff88990be3b10f375b10b8106328e8bf
jonty1231 <jontythakur68@gmail.com>
2025-12-20 12:08:28 +0530

    new apis

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ const fetch = require("node-fetch");
 const  cookieParser = require("cookie-parser")
 const app = express();
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: [ process.env.FRONTEND_URL1,
+      process.env.FRONTEND_URL2,
+      process.env.FRONTEND_URL3],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
